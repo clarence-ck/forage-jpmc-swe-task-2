c087776d4fb4f027d3b6f9d629beda5c8106c8ca
Clarence Sai <82635407+clarence-ck@users.noreply.github.com>
2024-08-12 00:03:43 +0800

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {Table} from '@finos/perspective';
-import {ServerRespond} from './DataStreamer';
+import React, { Component } from 'react';
+import { Table } from '@finos/perspective';
+import { ServerRespond } from './DataStreamer';
 import './Graph.css';
 
 /**
@@ -31,7 +31,7 @@ class Graph extends Component<IProps, {}> {
     }
 
     componentDidMount() {
-        // Get element to attach the table from the DOM.
+        // Get the Perspective viewer element from the DOM
         const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
         const schema = {
@@ -42,16 +42,16 @@ class Graph extends Component<IProps, {}> {
         };
 
         if (window.perspective && window.perspective.worker()) {
+            // Create the Perspective table with the defined schema
             this.table = window.perspective.worker().table(schema);
         }
+        
         if (this.table) {
-            // Load the `table` in the `<perspective-viewer>` DOM reference.
-
-            // Add more Perspective configurations here.
+            // Configure the Perspective viewer
             elem.load(this.table);
             elem.setAttribute("view", "y_line");
             elem.setAttribute("column-pivots", '["stock"]');
-            elem.setAttribute("row_pivots", '["timestamp"]');
+            elem.setAttribute("row-pivots", '["timestamp"]');
             elem.setAttribute("columns", '["top_ask_price"]');
             elem.setAttribute("aggregates",
                 '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
@@ -59,20 +59,25 @@ class Graph extends Component<IProps, {}> {
         }
     }
 
-    componentDidUpdate() {
-        // Everytime the data props is updated, insert the data into Perspective table
-        if (this.table) {
-            // As part of the task, you need to fix the way we update the data props to
-            // avoid inserting duplicated entries into Perspective table again.
-            this.table.update(this.props.data.map((el: any) => {
-                // Format the data from ServerRespond to the schema
-                return {
-                    stock: el.stock,
-                    top_ask_price: el.top_ask && el.top_ask.price || 0,
-                    top_bid_price: el.top_bid && el.top_bid.price || 0,
-                    timestamp: el.timestamp,
-                };
-            }));
+    componentDidUpdate(prevProps: IProps) {
+        // Only update the table if the data has changed
+        if (this.table && prevProps.data !== this.props.data) {
+            // Convert data to Perspective's expected format
+            const dataToUpdate = this.props.data.reduce((acc, el: any) => {
+                acc.stock.push(el.stock);
+                acc.top_ask_price.push(el.top_ask?.price || 0);
+                acc.top_bid_price.push(el.top_bid?.price || 0);
+                acc.timestamp.push(new Date(el.timestamp)); // Ensure the timestamp is a Date object
+                return acc;
+            }, {
+                stock: [] as string[],
+                top_ask_price: [] as number[],
+                top_bid_price: [] as number[],
+                timestamp: [] as Date[],
+            });
+
+            // Update the Perspective table with new data
+            this.table.update(dataToUpdate);
         }
     }
 }
